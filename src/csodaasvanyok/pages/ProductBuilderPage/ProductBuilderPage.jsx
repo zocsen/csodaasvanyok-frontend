8f8ba8ab07a188d53c290dc353cb86dc0959f406
zocsen <zocsen@gmail.com>
2023-11-24 15:58:21 +0100

    Little styling changes

--- src/csodaasvanyok/pages/ProductBuilderPage/ProductBuilderPage.jsx
+++ src/csodaasvanyok/pages/ProductBuilderPage/ProductBuilderPage.jsx
@@ -7,6 +7,10 @@ export default function ProductBuilderPage() {
         Ezen az oldalon hamarosan saját magad is összeállíthatod kedvenc
         karkötőid!
       </h2>
+      <h3 style={{ marginTop: "40px", textAlign: "center", color: "green" }}>
+        Ha egyedi karkötőt szeretnél, vedd fel a kapcsolatot velünk valamelyik
+        elérhetőségeink közül:
+      </h3>
     </div>
   );
 }
