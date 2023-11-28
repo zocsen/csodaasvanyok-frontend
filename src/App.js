b45c7d65e6fd913f9c76dba784428b3c8814fd67
zocsen <zocsen@gmail.com>
2023-11-28 19:59:18 +0100

    added helmet for the app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { CartProvider } from "./hooks/cartContext";
 import { DeliveryProvider } from "./hooks/deliveryContext";
 import { DataProvider } from "./hooks/dataContext";
 import { StripeProvider } from "./hooks/stripeContext";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 function App() {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 1024);
@@ -25,19 +26,31 @@ function App() {
   }, [isMobile]);
 
   return (
-    <IsMobileContext.Provider value={isMobile}>
-      <DataProvider>
-        <CartProvider>
-          <DeliveryProvider>
-            <StripeProvider>
-              <div className="App">
-                <Csodaasvanyok></Csodaasvanyok>
-              </div>
-            </StripeProvider>
-          </DeliveryProvider>
-        </CartProvider>
-      </DataProvider>
-    </IsMobileContext.Provider>
+    <HelmetProvider>
+      <IsMobileContext.Provider value={isMobile}>
+        <DataProvider>
+          <CartProvider>
+            <DeliveryProvider>
+              <StripeProvider>
+                <div className="App">
+                  <Csodaasvanyok></Csodaasvanyok>
+                  <Helmet>
+                    <title>
+                      CsodaÁsványok - Mi hiszünk az ásványok természetes
+                      erejében!
+                    </title>
+                    <meta
+                      name="description"
+                      content="Eredeti ásvány gyöngyökből készítünk minőségi ásvány karkötőket és egyéb ékszereket. Online ékszertervezőnk segítségével akár te is összeállíthatod álmaid ékszerét!"
+                    />
+                  </Helmet>
+                </div>
+              </StripeProvider>
+            </DeliveryProvider>
+          </CartProvider>
+        </DataProvider>
+      </IsMobileContext.Provider>
+    </HelmetProvider>
   );
 }
 
