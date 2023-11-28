bf7a97014293ea510404ac0c84fff1b6a5fdc289
zocsen <zocsen@gmail.com>
2023-11-28 15:19:30 +0100

    Added empty cart placeholder

--- src/csodaasvanyok/components/Cart/Cart.jsx
+++ src/csodaasvanyok/components/Cart/Cart.jsx
@@ -62,92 +62,109 @@ export default function Cart() {
             />
           </button>
         </div>
-        <ul className="cart-middle">
-          {cartItems.map((item) => {
-            return (
-              <li
-                className="cart-item"
-                key={`${item.id}_${item.size || "default"}`}
-              >
-                <img
-                  className="cart-item-image"
-                  src={item.image}
-                  alt=""
-                  width={100}
-                />
-                <div className="item-details-wrapper">
-                  <div className="upper-details">
-                    <div className="item-name-size-wrapper">
-                      <p className="item-name">{item.name}</p>
-                      {item.size && (
-                        <p className="item-size">Méret: {item.size}</p>
-                      )}
+        {cartItems.length > 0 ? (
+          <>
+            <ul className="cart-middle">
+              {cartItems.map((item) => {
+                return (
+                  <li
+                    className="cart-item"
+                    key={`${item.id}_${item.size || "default"}`}
+                  >
+                    <img
+                      className="cart-item-image"
+                      src={item.image}
+                      alt=""
+                      width={100}
+                    />
+                    <div className="item-details-wrapper">
+                      <div className="upper-details">
+                        <div className="item-name-size-wrapper">
+                          <p className="item-name">{item.name}</p>
+                          {item.size && (
+                            <p className="item-size">Méret: {item.size}</p>
+                          )}
+                        </div>
+                      </div>
+                      <div className="lower-details">
+                        <div className="item-quantity-wrapper">
+                          <button
+                            className="remove-item-button"
+                            onClick={() => decreaseQuantity(item)}
+                          >
+                            <RemoveIcon width={20} height={20} />
+                          </button>
+                          <p className="item-quantity">{item.quantity}</p>
+                          <button
+                            className="add-item-button"
+                            onClick={() => increaseQuantity(item)}
+                          >
+                            <AddIcon width={20} height={20} />
+                          </button>
+                        </div>
+                        <p className="item-price">
+                          {formatPrice(item.price * item.quantity)}
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                  <div className="lower-details">
-                    <div className="item-quantity-wrapper">
-                      <button
-                        className="remove-item-button"
-                        onClick={() => decreaseQuantity(item)}
-                      >
-                        <RemoveIcon width={20} height={20} />
-                      </button>
-                      <p className="item-quantity">{item.quantity}</p>
-                      <button
-                        className="add-item-button"
-                        onClick={() => increaseQuantity(item)}
-                      >
-                        <AddIcon width={20} height={20} />
-                      </button>
-                    </div>
-                    <p className="item-price">
-                      {formatPrice(item.price * item.quantity)}
-                    </p>
-                  </div>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
+                  </li>
+                );
+              })}
+            </ul>
 
-        <div className="cart-footer">
-          <div className="to-free-delivery">
-            {showProgress && (
-              <>
-                {remainingPrice > 0 && (
-                  <p className="free-delivery-reminder">
-                    Már csak {formatPrice(remainingPrice)} és ingyen házhoz
-                    visszük!
-                  </p>
+            <div className="cart-footer">
+              <div className="to-free-delivery">
+                {showProgress && (
+                  <>
+                    {remainingPrice > 0 && (
+                      <p className="free-delivery-reminder">
+                        Már csak {formatPrice(remainingPrice)} és ingyen házhoz
+                        visszük!
+                      </p>
+                    )}
+
+                    <StyledLinearProgress
+                      variant="determinate"
+                      value={progress}
+                    />
+
+                    <div className="min-max-price-needed">
+                      <p>0 Ft</p>
+                      <p>{formatPrice(freeDeliveryThreshold)} </p>
+                    </div>
+                  </>
                 )}
+                <p className="deliver-fee-title">
+                  Szállítás:
+                  <span className={deliveryFee === 0 ? "free" : undefined}>
+                    {deliveryFee > 0
+                      ? "Akár " + formatPrice(deliveryFee)
+                      : "Ingyenes"}
+                  </span>
+                </p>
+              </div>
 
-                <StyledLinearProgress variant="determinate" value={progress} />
-
-                <div className="min-max-price-needed">
-                  <p>0 Ft</p>
-                  <p>{formatPrice(freeDeliveryThreshold)} </p>
-                </div>
-              </>
-            )}
-            <p className="deliver-fee-title">
-              Szállítás:
-              <span className={deliveryFee === 0 ? "free" : undefined}>
-                {deliveryFee > 0 ? formatPrice(deliveryFee) : "Ingyenes"}
-              </span>
+              <div className="cart-footer-total">
+                <p>Összesített ár:</p>
+                <p>{formatPrice(totalPriceWithDeliveryFee)}</p>
+              </div>
+              <button
+                className="to-payment-info-button"
+                onClick={openDeliveryPanel}
+              >
+                Tovább a szállításhoz
+              </button>
+            </div>
+          </>
+        ) : (
+          <div className="cart-empty-disclaimer">
+            <h2>Úgy látszik, a kosarad magányos!</h2>
+            <p>
+              Töltsd meg kedvenceiddel, és máris készen állunk, hogy
+              kiszolgálhassunk. 🎁
             </p>
           </div>
-
-          <div className="cart-footer-total">
-            <p>Összesített ár:</p>
-            <p>{formatPrice(totalPriceWithDeliveryFee)}</p>
-          </div>
-          <button
-            className="to-payment-info-button"
-            onClick={openDeliveryPanel}
-          >
-            Tovább a szállításhoz
-          </button>
-        </div>
+        )}
       </div>
       <div
         className={`overlay ${isCartOpen ? "open" : ""}`}
