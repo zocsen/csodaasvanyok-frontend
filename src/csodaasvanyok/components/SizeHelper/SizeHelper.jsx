ac3c5d1e90674383ae42fec83f13b83804892acd
zocsen <zocsen@gmail.com>
2024-01-15 17:25:06 +0100

    added size helper text, changed the secondary font

--- src/csodaasvanyok/components/SizeHelper/SizeHelper.jsx
+++ src/csodaasvanyok/components/SizeHelper/SizeHelper.jsx
@@ -8,7 +8,43 @@ export default function SizeHelper({
   const SizeHelperContent = () => {
     return (
       <div className="size-helper-content">
-        <p>Hamarosan...</p>
+        <h2>Hogyan mérd le a karkötő méreted?</h2>
+        <br />
+        <p>
+          Egy mérőszalag vagy centi segítségével könnyedén meghatározhatod az
+          ideális karkötő méretet. Tekerjük a mérőszalagot szorosan, de nem
+          összeszorítva a csuklónk köré ahol a karkötőt hordani fogjuk.
+        </p>
+        <br />
+        <p>
+          Ennek hiányában egy cérnaszálat vagy egy papír csíkot is használhatsz:
+          tekerd körbe a csuklódon, tollal jelöld be, majd mérd le egy vonalzó
+          segítségével.
+        </p>
+        <br />
+        <h2>Válaszd ki a neked megfelelő méretet.</h2>
+        <br />
+        <p></p>
+        <ul>
+          <li>
+            <strong>XS</strong> - 13-14 cm
+          </li>
+          <li>
+            <strong>S</strong> - 15-16 cm
+          </li>
+          <li>
+            <strong>M</strong> - 17 cm
+          </li>
+          <li>
+            <strong>L</strong> - 18 cm
+          </li>
+          <li>
+            <strong>XL</strong> - 19 cm
+          </li>
+          <li>
+            <strong>XXL</strong> - 20-21 cm
+          </li>
+        </ul>
       </div>
     );
   };
