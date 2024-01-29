538a401c2bc1f9e7869f9f1b7ef0918b8129802e
zocsen <zocsen@gmail.com>
2024-01-29 21:36:55 +0100

    removed unused browser router

--- src/csodaasvanyok/Csodaasvanyok.jsx
+++ src/csodaasvanyok/Csodaasvanyok.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/Header/Header";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage/Homepage";
 import ProductsPage from "./pages/ProductsPage/ProductsPage";
 import ProductPage from "./pages/ProductPage/ProductPage";
